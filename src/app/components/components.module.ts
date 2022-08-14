e4a1d7b96635bf8586418b443e1410c746dd7c58
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-08-14 03:22:13 -0300

    add navbar and user card components

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,22 @@ import { CommonModule } from '@angular/common';
 import { LoadingComponent } from './loading/loading.component';
 import { IonicModule } from '@ionic/angular';
 import { MonthlySalesChartComponent } from './monthly-sales-chart/monthly-sales-chart.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserCardComponent } from './user-card/user-card.component';
 
 @NgModule({
-  declarations: [LoadingComponent, MonthlySalesChartComponent],
+  declarations: [
+    LoadingComponent,
+    MonthlySalesChartComponent,
+    NavbarComponent,
+    UserCardComponent,
+  ],
   imports: [CommonModule, IonicModule],
-  exports: [LoadingComponent, MonthlySalesChartComponent],
+  exports: [
+    LoadingComponent,
+    MonthlySalesChartComponent,
+    NavbarComponent,
+    UserCardComponent,
+  ],
 })
 export class ComponentsModule {}
