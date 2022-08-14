acf13a7f5c02c22793e160c0e7101aecdf0b5173
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-08-14 03:02:09 -0300

    add monthly sales chart component

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoadingComponent } from './loading/loading.component';
+import { IonicModule } from '@ionic/angular';
+import { MonthlySalesChartComponent } from './monthly-sales-chart/monthly-sales-chart.component';
+
+@NgModule({
+  declarations: [LoadingComponent, MonthlySalesChartComponent],
+  imports: [CommonModule, IonicModule],
+  exports: [LoadingComponent, MonthlySalesChartComponent],
+})
+export class ComponentsModule {}
